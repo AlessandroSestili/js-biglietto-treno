e03dae7e396c4bfb873036eeac7f0c6313d3aadb
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-08 16:25:47 +0200

    implemento modifiche

---
+++ TrainTicket.js
@@ -0,0 +1,9 @@
+var km = parseInt(prompt("Quanti kilometri devi percorrere?"));
+var yearsOld = parseInt(prompt("Quanti anni hai?"));
+
+var prezzoBiglietto = ;
+
+console.log(km , "kilometri");
+console.log(yearsOld , "anni");
+
+
