dcc07f2eac9c6d41d27611b4de876af553e3ece5
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-08 17:33:38 +0200

    finisco progetto

--- TrainTicket.js
+++ TrainTicket.js
@@ -1,20 +1,22 @@
 var km = parseInt(prompt("Quanti kilometri devi percorrere?"));
 var yearsOld = parseInt(prompt("Quanti anni hai?"));
+var prezzoBiglietto = (km * 0.21);
 
-
-console.log(km , "kilometri");
-console.log(yearsOld , "anni");
-
-var prezzoBiglietto = (km * 0.21 + "$");
-console.log(prezzoBiglietto , "questo è il costo del tuo biglietto")
 
 if (yearsOld < 18) {
-    prezzoBiglietto = (km * 0.21+(10) + "$") 
+    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto*20)/ 100) ) 
 } 
 
 if (yearsOld >= 65) {
-    prezzoBiglietto = (km * 0.21+(100) + "$") 
-} 
+    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto*40)/100) ) 
+}
 
+console.log(km , "kilometri");
+console.log(yearsOld , "anni");
+console.log(prezzoBiglietto , "questo è il costo del tuo biglietto")
 
+if (Number.isNaN(km) || Number.isNaN(yearsOld)) {
+    alert("Sei uno stron**, metti dei ca**o di numeri!")
+}
 
+document.getElementById("js-container").innerHTML = "Il prezzo del tuo biglietto è:" + " " + prezzoBiglietto + "$"
