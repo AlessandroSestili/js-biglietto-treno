d33e7f7285d27e3841bfbc51be815de652c36309
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-08 16:35:12 +0200

    implemento if

--- TrainTicket.js
+++ TrainTicket.js
@@ -1,9 +1,20 @@
 var km = parseInt(prompt("Quanti kilometri devi percorrere?"));
 var yearsOld = parseInt(prompt("Quanti anni hai?"));
 
-var prezzoBiglietto = ;
 
 console.log(km , "kilometri");
 console.log(yearsOld , "anni");
+
+var prezzoBiglietto = (km * 0.21 + "$");
+console.log(prezzoBiglietto , "questo è il costo del tuo biglietto")
+
+if (yearsOld < 18) {
+    prezzoBiglietto = (km * 0.21+(10) + "$") 
+} 
+
+if (yearsOld >= 65) {
+    prezzoBiglietto = (km * 0.21+(100) + "$") 
+} 
+
 
 
