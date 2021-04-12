74828cad84747169356b981d9bebf386b78fd1d9
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-12 14:08:00 +0200

    finisco es3

--- TrainTicket.js
+++ TrainTicket.js
@@ -1,22 +1,26 @@
 var km = parseInt(prompt("Quanti kilometri devi percorrere?"));
 var yearsOld = parseInt(prompt("Quanti anni hai?"));
-var prezzoBiglietto = (km * 0.21);
+var prezzoAlKm = 0.21;
+var prezzoBiglietto = (km * prezzoAlKm);
 
 
 if (yearsOld < 18) {
-    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto*20)/ 100) ) 
+    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.20) ).toFixed(2) 
 } 
 
 if (yearsOld >= 65) {
-    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto*40)/100) ) 
+    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.40) ).toFixed(2) 
 }
 
 console.log(km , "kilometri");
 console.log(yearsOld , "anni");
-console.log(prezzoBiglietto , "questo è il costo del tuo biglietto")
+console.log(prezzoBiglietto , "questo è il costo del tuo biglietto");
 
-if (Number.isNaN(km) || Number.isNaN(yearsOld)) {
-    alert("Sei uno stron**, metti dei ca**o di numeri!")
+if (Number.isNaN(km) || Number.isNaN(yearsOld)); {
+    alert("Dati inesatti: inserisci dei numeri.")
 }
 
-document.getElementById("js-container").innerHTML = "Il prezzo del tuo biglietto è:" + " " + prezzoBiglietto + "$"
+// innesto HTML in #js-container
+document.getElementById("js-container").innerHTML = "Il prezzo del tuo biglietto è:" + " " + prezzoBiglietto + " " + "$";
+
+
